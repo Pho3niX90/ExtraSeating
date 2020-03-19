0e274fb900d17755bb2006782abaa5ffbf58642d
Shaun Jansen Van Nieuwenhuizen (Master) <shaun@jvn.sx>
2020-03-19 22:11:07 +0200

    * Change horse seat to static mount, to temporarily fix bugs

--- ExtraSeating.cs
+++ ExtraSeating.cs
@@ -1,50 +1,57 @@
+using Oxide.Core;
 using System;
 using UnityEngine;
 
-namespace Oxide.Plugins
-{
-    [Info("Extra Seating", "Pho3niX90", "1.0.0")]
+namespace Oxide.Plugins {
+    [Info("Extra Seating", "Pho3niX90", "1.0.3")]
     [Description("Allows extra seats on minicopters and horses")]
-    class ExtraSeating : RustPlugin
-    {
+    class ExtraSeating : RustPlugin {
         #region Config
         public PluginConfig config;
         static ExtraSeating _instance;
+        bool debug = false;
         int seats = 0;
+        string CHAIR_PREFAB = "assets/prefabs/vehicle/seats/passengerchair.prefab";
+        string INVIS_CHAIR_PREFAB = "assets/bundled/prefabs/static/chair.invisible.static.prefab";
 
         protected override void LoadDefaultConfig() { Config.WriteObject(GetDefaultConfig(), true); }
         public PluginConfig GetDefaultConfig() { return new PluginConfig { EnableMiniSideSeats = true, EnableMiniBackSeat = true, EnableExtraHorseSeat = true }; }
         public class PluginConfig { public bool EnableMiniSideSeats; public bool EnableMiniBackSeat; public bool EnableExtraHorseSeat; }
         #endregion
-
-        void OnEntitySpawned(BaseNetworkable entity)
-        {
+        private void Init() {
+            config = Config.ReadObject<PluginConfig>();
+        }
+        void LogDebug(string str) {
+            if (debug) Puts(str);
+        }
+        void OnEntitySpawned(BaseNetworkable entity) {
             _instance = this;
             if (entity == null || !(entity is MiniCopter || entity is RidableHorse)) return;
-
             BaseVehicle vehicle = entity as BaseVehicle;
             seats = vehicle.mountPoints.Length; // default
 
-            if (entity is MiniCopter)
-            {
+            if (entity is MiniCopter && entity.ShortPrefabName.Equals("minicopter.entity")) {
+
                 if (_instance.config.EnableMiniSideSeats) seats += 2;
                 if (_instance.config.EnableMiniBackSeat) seats += 1;
 
                 if (vehicle.mountPoints.Length < seats)
                     vehicle?.gameObject.AddComponent<Seating>();
             }
-            if (entity is RidableHorse)
-            {
+
+            if (entity is RidableHorse) {
                 if (_instance.config.EnableExtraHorseSeat) seats += 1;
 
                 if (vehicle.mountPoints.Length < seats)
-                    vehicle?.gameObject.AddComponent<Seating>();
+                    NextTick(() => {
+                        vehicle?.gameObject.AddComponent<Seating>();
+                    });
             }
         }
 
-        void AddSeat(BaseVehicle ent, Vector3 locPos, Quaternion q)
-        {
-            BaseEntity seat = GameManager.server.CreateEntity("assets/prefabs/vehicle/seats/passengerchair.prefab", ent.transform.position, q) as BaseEntity;
+        void AddSeat(BaseVehicle ent, Vector3 locPos, Quaternion q) {
+            string PREFAB = ent is MiniCopter ? CHAIR_PREFAB : INVIS_CHAIR_PREFAB;
+            BaseEntity seat = GameManager.server.CreateEntity(PREFAB, ent.transform.position, q) as BaseEntity;
             if (seat == null) return;
 
             seat.SetParent(ent);
@@ -53,78 +60,82 @@ namespace Oxide.Plugins
             seat.SendNetworkUpdateImmediate(true);
         }
 
-        BaseVehicle.MountPointInfo CreateMount(Vector3 vec, BaseVehicle.MountPointInfo exampleSeat, Vector3 rotation)
-        {
-            return new BaseVehicle.MountPointInfo
-            {
+        BaseVehicle.MountPointInfo CreateMount(Vector3 vec, BaseVehicle.MountPointInfo exampleSeat) {
+            return CreateMount(vec, exampleSeat, new Vector3());
+        }
+
+        BaseVehicle.MountPointInfo CreateMount(Vector3 vec, BaseVehicle.MountPointInfo exampleSeat, Vector3 rotation) {
+            return new BaseVehicle.MountPointInfo {
                 pos = vec,
                 rot = rotation != null ? rotation : new Vector3(0, 0, 0),
+                bone = exampleSeat.bone,
                 prefab = exampleSeat.prefab,
                 mountable = exampleSeat.mountable
             };
         }
 
         #region Classes
-        class Seating : MonoBehaviour
-        {
+        class Seating : FacepunchBehaviour {
             public BaseVehicle entity;
-            void Awake()
-            {
+            void Awake() {
                 entity = GetComponent<BaseVehicle>();
+                if (entity == null) { Destroy(this); return; }
+
                 bool isMini = entity is MiniCopter;
                 bool isHorse = entity is RidableHorse;
+                Vector3 emptyVector = new Vector3(0, 0, 0);
 
-                if (isMini)
-                {
-                    _instance.Puts("Minicopter detected");
+                if (isMini) {
+                    _instance.LogDebug("Minicopter detected");
                 }
-                if (isHorse)
-                {
-                    _instance.Puts("Horse detected");
+                if (isHorse) {
+                    _instance.LogDebug("Horse detected");
                 }
-
-                if (entity == null) { Destroy(this); return; }
 
                 BaseVehicle.MountPointInfo pilot = entity.mountPoints[0];
 
-                Array.Resize(ref entity.mountPoints, _instance.seats);
-                if (entity is RidableHorse)
-                {
-                    Vector3 horseVector = new Vector3(0f, 1.2f, -0.7f);
-                    BaseVehicle.MountPointInfo horseBack = _instance.CreateMount(horseVector, pilot, new Vector3(0, 0, 0));
-                    entity.mountPoints[1] = horseBack;
+
+                if (isHorse) {
+                    _instance.LogDebug("Adding passenger seat");
+                    //Vector3 horseVector = new Vector3(0f, -0.32f, -0.5f);
+                    Vector3 horseVector2 = new Vector3(0f, 1.0f, -0.5f);
+                    //BaseVehicle.MountPointInfo horseBack = _instance.CreateMount(horseVector, pilot);
+                    //entity.mountPoints[0] = pilot;
+                    //entity.mountPoints[1] = horseBack;
+                    _instance.AddSeat(entity, horseVector2, new Quaternion());
                 }
 
-                if (entity is MiniCopter)
-                {
-
+                if (isMini) {
+                    Array.Resize(ref entity.mountPoints, _instance.seats);
                     BaseVehicle.MountPointInfo pFront = entity.mountPoints[1];
                     Vector3 leftVector = new Vector3(0.6f, 0.2f, -0.2f);
                     Vector3 rightVector = new Vector3(-0.6f, 0.2f, -0.2f);
-                    Vector3 backVector = new Vector3(0.0f, 0.4f, -1.45f);
+                    Vector3 backVector = new Vector3(0.0f, 0.4f, -1.2f);
+                    Vector3 backVector2 = new Vector3(0.0f, 0.4f, -1.45f);
+
                     Vector3 playerOffsetVector = new Vector3(0f, 0f, -0.25f);
                     Quaternion backQuaternion = Quaternion.Euler(0f, 180f, 0f);
 
-                    if (_instance.config.EnableMiniSideSeats)
-                    {
-                        BaseVehicle.MountPointInfo pLeftSide = _instance.CreateMount(leftVector, pFront, new Vector3(0, 0, 0));
-                        BaseVehicle.MountPointInfo pRightSide = _instance.CreateMount(rightVector, pFront, new Vector3(0, 0, 0));
+                    if (_instance.config.EnableMiniSideSeats) {
+                        _instance.LogDebug("Adding side seats");
+                        BaseVehicle.MountPointInfo pLeftSide = _instance.CreateMount(leftVector, pFront);
+                        BaseVehicle.MountPointInfo pRightSide = _instance.CreateMount(rightVector, pFront);
                         entity.mountPoints[2] = pLeftSide;
-
-                        _instance.AddSeat(entity, leftVector + playerOffsetVector, new Quaternion());
                         entity.mountPoints[3] = pRightSide;
+                        _instance.AddSeat(entity, leftVector + playerOffsetVector, new Quaternion());
                         _instance.AddSeat(entity, rightVector + playerOffsetVector, new Quaternion());
                     }
 
-                    if (_instance.config.EnableMiniBackSeat)
-                    {
-                        BaseVehicle.MountPointInfo pBackReverse = _instance.CreateMount(backVector, pFront, new Vector3(0, 0, 0));
+                    if (_instance.config.EnableMiniBackSeat) {
+                        _instance.LogDebug("Adding back/rotor seat");
+                        BaseVehicle.MountPointInfo pBackReverse = _instance.CreateMount(backVector2, pFront, new Vector3(0f, 180f, 0f));
                         entity.mountPoints[_instance.seats - 1] = pBackReverse;
-                        _instance.AddSeat(entity, backVector + playerOffsetVector, backQuaternion);
+                        _instance.AddSeat(entity, backVector, backQuaternion);
                     }
                 }
 
             }
+
         }
         #endregion
     }
